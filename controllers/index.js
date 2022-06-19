4e96275f2aecf25870a9eca2f50fae9304843e5a
bcheung827 <83896907+bcheung827@users.noreply.github.com>
2022-06-18 20:01:46 -0400

    added code to controllers folder

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const apiRoutes = require('./apis');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+router.use('/apis', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
